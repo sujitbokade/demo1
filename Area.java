cdf1e82e8a1be1aca38c04cb8d4f15ea4de81a32
sujit <sujitbokade2014@gmail.com>
2022-06-15 22:03:58 +0530

    [Sujit]Modified Program

--- Area.java
+++ Area.java
@@ -4,14 +4,14 @@ public class Area{
        public static void main(String[] args){
        Scanner input  = new Scanner(System.in);
 
-       System.out.println("Enter length of plot: ");
+       System.out.println("Enter length of plot in Feet: ");
         double length = input.nextDouble();
-       System.out.println("Enter breadth of plot: ");
+       System.out.println("Enter breadth of plot in Feet: ");
         double breadth = input.nextDouble();
         double area = length*breadth;
         double Area = area/10.76;
 
-      System.out.println("Area of rectangular in Sqm:"+Area);
+      System.out.println("Area of rectangular in Square Meter:"+Area);
 
 
 
